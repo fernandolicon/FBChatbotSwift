f8712371e7f3d622a0805a2787975de5324400c3
Luis Fernando Mata <fernando@reigndesign.com>
2017-05-29 17:20:03 +0800

    Remove init for Message struct, for debugging of message sent by Facebook

--- Sources/App/Models.swift
+++ Sources/App/Models.swift
@@ -18,15 +18,7 @@ struct Message {
     let sendJson: [String: AnyObject]
     
     init(message: JSON) {
-        let senderID: String = (message["sender"]?["id"]?.string)!
-        if let textString = message["message"]?["text"]?.string {
-            self.textString = textString
-        }
         
-        let idJson: [String:String] = ["id": senderID]
-        let textJson: [String: String] = ["text": self.textString]
-        
-        self.sendJson = ["recipient": idJson as AnyObject, "message": textJson as AnyObject]
     }
     
 }
